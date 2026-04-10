0648d62cc175d17572d0d7c95581bad9759d5a5f
matheus <matheusdias.front96@gmail.com>
2026-04-10 18:46:50 -0300

    chore / functions: remove unused send whatsapp function

--- src/main/functions/sendWhatsAppMessage.ts
+++
@@ -1,5 +0,0 @@
-import 'reflect-metadata';
-import { SendWhatsAppMessageController } from '@application/controller/SendWhatsAppMessageController';
-import { lambdaHttpAdapter } from '@main/adapters/lambdaHttpAdapter';
-
-export const handler = lambdaHttpAdapter(SendWhatsAppMessageController);
